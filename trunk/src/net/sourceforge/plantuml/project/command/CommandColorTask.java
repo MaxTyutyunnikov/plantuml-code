8bf0138be3c6c38fa188ac45c96b1f02a09235d6
arnaud_roques <arnaud_roques@2bfd43ce-aac2-44f9-bcbc-61bf0d4e0ab5>
2020-04-26 14:53:59 +0000

    Version 1.2020.8

--- trunk/src/net/sourceforge/plantuml/project/command/CommandColorTask.java
+++ trunk/src/net/sourceforge/plantuml/project/command/CommandColorTask.java
@@ -33,51 +33,54 @@
  * 
  *
  */
-package net.sourceforge.plantuml.command;
+package net.sourceforge.plantuml.project.command;
 
-import net.sourceforge.plantuml.AbstractPSystem;
 import net.sourceforge.plantuml.LineLocation;
-import net.sourceforge.plantuml.ScaleSimple;
+import net.sourceforge.plantuml.command.CommandExecutionResult;
+import net.sourceforge.plantuml.command.SingleLineCommand2;
 import net.sourceforge.plantuml.command.regex.IRegex;
 import net.sourceforge.plantuml.command.regex.RegexConcat;
 import net.sourceforge.plantuml.command.regex.RegexLeaf;
-import net.sourceforge.plantuml.command.regex.RegexOptional;
 import net.sourceforge.plantuml.command.regex.RegexResult;
+import net.sourceforge.plantuml.project.Failable;
+import net.sourceforge.plantuml.project.GanttConstraint;
+import net.sourceforge.plantuml.project.GanttDiagram;
+import net.sourceforge.plantuml.project.core.Task;
+import net.sourceforge.plantuml.project.core.TaskAttribute;
+import net.sourceforge.plantuml.project.core.TaskInstant;
+import net.sourceforge.plantuml.project.lang.Complement;
+import net.sourceforge.plantuml.project.lang.ComplementColors;
+import net.sourceforge.plantuml.ugraphic.color.HColor;
 
-public class CommandScale extends SingleLineCommand2<AbstractPSystem> {
+public class CommandColorTask extends SingleLineCommand2<GanttDiagram> {
 
-	public CommandScale() {
+	public CommandColorTask() {
 		super(getRegexConcat());
 	}
 
 	static IRegex getRegexConcat() {
-		return RegexConcat.build(CommandScale.class.getName(), RegexLeaf.start(), //
-				new RegexLeaf("scale"), //
+		return RegexConcat.build(CommandColorTask.class.getName(), RegexLeaf.start(), //
+				new RegexLeaf("CODE", "\\[([\\p{L}0-9_.]+)\\]"), //
 				RegexLeaf.spaceOneOrMore(), //
-				new RegexLeaf("SCALE", "([0-9.]+)"), //
-				new RegexOptional( //
-						new RegexConcat( //
-								RegexLeaf.spaceZeroOrMore(), //
-								new RegexLeaf("/"), //
-								RegexLeaf.spaceZeroOrMore(), //
-								new RegexLeaf("DIV", "([0-9.]+)") //
-						)), RegexLeaf.end()); //
+				new RegexLeaf("COLORS", "#(\\w+)(?:/(#?\\w+))?"), //
+				RegexLeaf.spaceZeroOrMore(), RegexLeaf.end());
 	}
 
 	@Override
-	protected CommandExecutionResult executeArg(AbstractPSystem diagram, LineLocation location, RegexResult arg) {
-		double scale = Double.parseDouble(arg.get("SCALE", 0));
-		if (scale == 0) {
-			return CommandExecutionResult.error("Scale cannot be zero");
+	protected CommandExecutionResult executeArg(GanttDiagram diagram, LineLocation location, RegexResult arg) {
+
+		final String code = arg.get("CODE", 0);
+		final Task task = diagram.getExistingTask(code);
+		if (task == null) {
+			return CommandExecutionResult.error("No such task " + code);
 		}
-		if (arg.get("DIV", 0) != null) {
-			final double div = Double.parseDouble(arg.get("DIV", 0));
-			if (div == 0) {
-				return CommandExecutionResult.error("Scale cannot be zero");
-			}
-			scale /= div;
-		}
-		diagram.setScale(new ScaleSimple(scale));
+
+		final String color1 = arg.get("COLORS", 0);
+		final String color2 = arg.get("COLORS", 1);
+		final HColor col1 = diagram.getIHtmlColorSet().getColorIfValid(color1);
+		final HColor col2 = diagram.getIHtmlColorSet().getColorIfValid(color2);
+		task.setColors(new ComplementColors(col1, col2));
+
 		return CommandExecutionResult.ok();
 	}
 
