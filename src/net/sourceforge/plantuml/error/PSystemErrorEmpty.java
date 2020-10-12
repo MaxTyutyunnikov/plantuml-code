972f274129a99090a87e6271feb720818d5f7c5b
arnaud_roques <arnaud_roques@2bfd43ce-aac2-44f9-bcbc-61bf0d4e0ab5>
2020-10-12 17:29:35 +0000

    Version 1.2020.19

--- src/net/sourceforge/plantuml/error/PSystemErrorEmpty.java
+++ src/net/sourceforge/plantuml/error/PSystemErrorEmpty.java
@@ -30,50 +30,23 @@
  *
  *
  * Original Author:  Arnaud Roques
- * 
  *
  */
-package net.sourceforge.plantuml.command;
+package net.sourceforge.plantuml.error;
 
 import java.util.List;
 
 import net.sourceforge.plantuml.ErrorUml;
-import net.sourceforge.plantuml.ErrorUmlType;
-import net.sourceforge.plantuml.LineLocation;
 import net.sourceforge.plantuml.StringLocated;
-import net.sourceforge.plantuml.api.PSystemFactory;
-import net.sourceforge.plantuml.core.DiagramType;
 import net.sourceforge.plantuml.core.UmlSource;
-import net.sourceforge.plantuml.error.PSystemError;
-import net.sourceforge.plantuml.error.PSystemErrorUtils;
 
-public abstract class PSystemAbstractFactory implements PSystemFactory {
+public class PSystemErrorEmpty extends PSystemError {
 
-	public static final String EMPTY_DESCRIPTION = "Empty description";
-	private final DiagramType type;
+	public PSystemErrorEmpty(UmlSource source, List<StringLocated> trace, ErrorUml singleError) {
+		this.setSource(source);
+		this.trace = trace;
+		this.singleError = singleError;
 
-	protected PSystemAbstractFactory(DiagramType type) {
-		this.type = type;
-	}
-
-	final protected PSystemError buildEmptyError(UmlSource source, LineLocation lineLocation,
-			List<StringLocated> trace) {
-		final ErrorUml err = new ErrorUml(ErrorUmlType.SYNTAX_ERROR, EMPTY_DESCRIPTION, lineLocation);
-		final PSystemError result = PSystemErrorUtils.buildV2(source, err, null, trace);
-		result.setSource(source);
-		return result;
-	}
-
-	final protected PSystemError buildExecutionError(UmlSource source, String stringError, LineLocation lineLocation,
-			List<StringLocated> trace) {
-		final ErrorUml err = new ErrorUml(ErrorUmlType.EXECUTION_ERROR, stringError, lineLocation);
-		final PSystemError result = PSystemErrorUtils.buildV2(source, err, null, trace);
-		result.setSource(source);
-		return result;
-	}
-
-	final public DiagramType getDiagramType() {
-		return type;
 	}
 
 }
