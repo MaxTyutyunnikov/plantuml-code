48e1425e684978e8582e18d8e694359a7127a3b9
arnaud_roques <arnaud_roques@2bfd43ce-aac2-44f9-bcbc-61bf0d4e0ab5>
2022-03-29 17:52:55 +0000

    Version 1.2022.3

--- src/net/sourceforge/plantuml/style/StyleLoader.java
+++ src/net/sourceforge/plantuml/style/StyleLoader.java
@@ -88,8 +88,12 @@ public class StyleLoader {
 		InputStream internalIs = null;
 		SFile localFile = new SFile(filename);
 		Log.info("Trying to load style " + filename);
-		if (localFile.exists() == false)
-			localFile = FileSystem.getInstance().getFile(filename);
+		try {
+			if (localFile.exists() == false)
+				localFile = FileSystem.getInstance().getFile(filename);
+		} catch (IOException e) {
+			Log.info("Cannot open file. " + e);
+		}
 
 		if (localFile.exists()) {
 			Log.info("File found : " + localFile.getPrintablePath());
